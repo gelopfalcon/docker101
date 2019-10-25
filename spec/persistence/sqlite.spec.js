700e0c509949a4e945246da990af3c7c18cc71ff
Gerardo Lopez Falcon <gelopfalcon@gmail.com>
2019-10-25 08:31:56 -0600

    Upload project

---
+++ spec/persistence/sqlite.spec.js
@@ -0,0 +1,60 @@
+const db = require('../../src/persistence/sqlite');
+const fs = require('fs');
+
+const ITEM = {
+    id: '7aef3d7c-d301-4846-8358-2a91ec9d6be3',
+    name: 'Test',
+    completed: false,
+};
+
+afterEach(() => {
+    if (fs.existsSync('/tmp/todo.db')) {
+        fs.unlinkSync('/tmp/todo.db');
+    }
+});
+
+test('it initializes correctly', async () => {
+    await db.init();
+});
+
+test('it can store and retrieve items', async () => {
+    await db.init();
+
+    await db.storeItem(ITEM);
+
+    const items = await db.getItems();
+    expect(items.length).toBe(1);
+    expect(items[0]).toEqual(ITEM);
+});
+
+test('it can update an existing item', async () => {
+    await db.init();
+    await db.storeItem(ITEM);
+
+    await db.updateItem(
+        ITEM.id,
+        Object.assign({}, ITEM, { completed: !ITEM.completed }),
+    );
+
+    const items = await db.getItems();
+    expect(items.length).toBe(1);
+    expect(items[0].completed).toBe(!ITEM.completed);
+});
+
+test('it can remove an existing item', async () => {
+    await db.init();
+    await db.storeItem(ITEM);
+
+    await db.removeItem(ITEM.id);
+
+    const items = await db.getItems();
+    expect(items.length).toBe(0);
+});
+
+test('it can get a single item', async () => {
+    await db.init();
+    await db.storeItem(ITEM);
+
+    const item = await db.getItem(ITEM.id);
+    expect(item).toEqual(ITEM);
+});
---
+++ src/persistence/mysql.js
@@ -0,0 +1,116 @@
+const mysql = require('mysql');
+
+const {
+    MYSQL_HOST: HOST,
+    MYSQL_USER: USER,
+    MYSQL_PASSWORD: PASSWORD,
+    MYSQL_DB: DB,
+} = process.env;
+
+let pool;
+
+async function init() {
+    pool = mysql.createPool({
+        connectionLimit: 5,
+        host: HOST,
+        user: USER,
+        password: PASSWORD,
+        database: DB,
+    });
+
+    return new Promise((acc, rej) => {
+        pool.query(
+            'CREATE TABLE IF NOT EXISTS todo_items (id varchar(36), name varchar(255), completed boolean)',
+            err => {
+                if (err) return rej(err);
+
+                console.log(`Connected to mysql db at host ${HOST}`);
+                acc();
+            },
+        );
+    });
+}
+
+async function teardown() {
+    return new Promise((acc, rej) => {
+        pool.end(err => {
+            if (err) rej(err);
+            else acc();
+        });
+    });
+}
+
+async function getItems() {
+    return new Promise((acc, rej) => {
+        pool.query('SELECT * FROM todo_items', (err, rows) => {
+            if (err) return rej(err);
+            acc(
+                rows.map(item =>
+                    Object.assign({}, item, {
+                        completed: item.completed === 1,
+                    }),
+                ),
+            );
+        });
+    });
+}
+
+async function getItem(id) {
+    return new Promise((acc, rej) => {
+        pool.query('SELECT * FROM todo_items WHERE id=?', [id], (err, rows) => {
+            if (err) return rej(err);
+            acc(
+                rows.map(item =>
+                    Object.assign({}, item, {
+                        completed: item.completed === 1,
+                    }),
+                )[0],
+            );
+        });
+    });
+}
+
+async function storeItem(item) {
+    return new Promise((acc, rej) => {
+        pool.query(
+            'INSERT INTO todo_items (id, name, completed) VALUES (?, ?, ?)',
+            [item.id, item.name, item.completed ? 1 : 0],
+            err => {
+                if (err) return rej(err);
+                acc();
+            },
+        );
+    });
+}
+
+async function updateItem(id, item) {
+    return new Promise((acc, rej) => {
+        pool.query(
+            'UPDATE todo_items SET name=?, completed=? WHERE id=?',
+            [item.name, item.completed ? 1 : 0, id],
+            err => {
+                if (err) return rej(err);
+                acc();
+            },
+        );
+    });
+}
+
+async function removeItem(id) {
+    return new Promise((acc, rej) => {
+        pool.query('DELETE FROM todo_items WHERE id = ?', [id], err => {
+            if (err) return rej(err);
+            acc();
+        });
+    });
+}
+
+module.exports = {
+    init,
+    teardown,
+    getItems,
+    getItem,
+    storeItem,
+    updateItem,
+    removeItem,
+};
